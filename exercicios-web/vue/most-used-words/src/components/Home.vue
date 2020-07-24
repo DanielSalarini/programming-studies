8710464a4c0fdde2342c564f85a71477dda239e1
Daniel Salarini <salarini.robotica@gmail.com>
2020-07-24 15:42:10 -0300

    Vue class with Electron project most-used-words backend

--- exercicios-web/vue/most-used-words/src/components/Home.vue
+++ exercicios-web/vue/most-used-words/src/components/Home.vue
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import { ipcRenderer } from 'electron'
+
 import Pill from './Pill'
 
 export default {
@@ -27,17 +29,17 @@ export default {
     data: function() {
         return {
             files: [],
-            groupedWords: [
-                { name: 'i', amount: 1234 },
-                { name: 'you', amount: 900 },
-                { name: 'he', amount: 853 },
-            ]
+            groupedWords: []
         }
     },
 
     methods: {
         processSubTitles() {
-            console.log(this.files)
+            const paths = this.files.map(f => f.path)
+            ipcRenderer.send('process-subtitles', paths)
+            ipcRenderer.on('process-subtitles', (event, resp) => {
+                this.groupedWords = resp
+            })
         }
     }
 }
