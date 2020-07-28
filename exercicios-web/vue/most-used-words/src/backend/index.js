48797792b7bf3e862c645e8c70297cfbc1cc9020
Daniel Salarini <salarini.robotica@gmail.com>
2020-07-28 14:26:48 -0300

    Vue class Electron project most-used-words complet

--- exercicios-web/vue/most-used-words/src/backend/index.js
+++ exercicios-web/vue/most-used-words/src/backend/index.js
@@ -1,14 +1,14 @@
 const { ipcMain } = require('electron')
 
+const pathsToRows = require('./pathsToRow')
+const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
+
 ipcMain.on('process-subtitles', (event, paths) => {
     console.log(paths)
 
-    event.reply('process-subtitles', [
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'she', amount: 1234 },
-        { name: 'we', amount: 900 },
-        { name: 'they', amount: 853 },
-    ])
+    pathsToRows(paths)
+        .then(rows => prepareData(rows))
+        .then(words => groupWords(words))
+        .then(groupedWords => event.reply('process-subtitles', groupedWords))
 })
