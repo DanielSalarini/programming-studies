93810cc582d785ef5c11bd73d8ce1c853a8fe987
Daniel Salarini <salarini.robotica@gmail.com>
2020-08-10 16:11:39 -0300

    Chapter node correction

--- exercicios-web/node/pastaA/pastaB/acessarModulos.js
+++ exercicios-web/node/pastaA/pastaB/acessarModulos.js
@@ -1,12 +1,16 @@
+// exemplo de require de arquivo para arquivo
 const moduloA = require('../../moduloA')
 console.log(moduloA.ola)
 
-const saudacao = require('saudacao')
-console.log(saudacao.ola)
+// exemplo de require do node_modules/terceiros
+// const saudacao = require('saudacao')
+// console.log(saudacao.ola)
 
+// exemplo de require de padrao index.js do modulo
 const c = require('./pastaC')
 console.log(c.ola2)
 
+// exemplo de require de module core do node
 const http = require('http')
 http.createServer((req, res) => {
     res.write('Bom dia!')
