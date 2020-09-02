2d8820516e02f82e1264840a582223abb6521ed0
Daniel Salarini <salarini.robotica@gmail.com>
2020-09-02 20:14:25 -0300

    Node class

--- exercicios-web/node/temporizador.js
+++ exercicios-web/node/temporizador.js
@@ -1,5 +1,5 @@
 const schedule = require('node-schedule')
-const tarefa1 = schedule.scheduleJob('*/5 * 17 * * 4', function () {
+const tarefa1 = schedule.scheduleJob('*/5 * 20 * * 3', function () {
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
 
@@ -10,7 +10,7 @@ setTimeout(function () {
 
 const regra = new schedule.RecurrenceRule()
 regra.dayOfWeek = [new schedule.Range(1, 5)]
-regra.hour = 17
+regra.hour = 20
 regra.second = 30
 
 const tarefa2 = schedule.scheduleJob(regra, function() {
