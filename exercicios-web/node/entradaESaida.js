2a4f8d5d0148f3708ab038f9b89044b9e5b60a12
Daniel Salarini <salarini.robotica@gmail.com>
2020-09-02 20:22:11 -0300

    Node class #2

--- exercicios-web/node/entradaESaida.js
+++ exercicios-web/node/entradaESaida.js
@@ -2,14 +2,14 @@ const anonimo = process.argv.indexOf('-a') !== -1
 // console.log(anonimo)
 
 if(anonimo) {
-    process.stdout.write('Fala Anonimo!\n')
+    process.stdout.write('Fala Anonimo!')
     process.exit()
 } else {
     process.stdout.write('Informe o seu nome: ')
     process.stdin.on('data', data => {
-        const nome = data.toString().replace('\n', ' ')
+        const nome = data.toString()
 
-        process.stdout.write(`Fala ${nome}!!\n`)
+        process.stdout.write(`Fala ${nome}!!`)
         process.exit()
     })
 }
