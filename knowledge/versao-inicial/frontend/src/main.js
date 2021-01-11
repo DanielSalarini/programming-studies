c051a235576f7eb6e3d712fecd211ec02621da23
Daniel Salarini <salarini.robotica@gmail.com>
2021-01-11 16:28:50 -0300

    Knowledge Frontend #2

--- knowledge/versao-inicial/frontend/src/main.js
+++ knowledge/versao-inicial/frontend/src/main.js
@@ -1,11 +1,20 @@
 import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
+
 import App from './App'
+
+import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+//Temporario
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAsIm5hbWUiOiJK'
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
